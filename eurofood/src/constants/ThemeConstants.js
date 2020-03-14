8d7fe9f74b8c03eca4a7b738bbdcd5dcdf5926dc
Marco Abate <m.abate@logispin.com>
2020-03-14 15:53:55 +0100

    colors

--- eurofood/src/constants/ThemeConstants.js
+++ eurofood/src/constants/ThemeConstants.js
@@ -1,9 +1,16 @@
 import Color from 'color';
 
 const black = Color('#000000');
-const white = Color('#FFFFFF');
-const teal = Color('#008080');
-const firebrick = Color('#B22222');
+const white = Color('#3d3d3d');
+const orange = Color('#fe9c00');
+const lightOrange = Color('#f4a400');
+const darkOrange = Color('#f34c00');
+const dark = Color('#ffffff');
+const red = Color('#ae2018');
+const darkRed = Color('#da3215');
+const gray = Color('#7f7f7f');
+const lightGray = Color('#f5f5f5');
+const azure = Color('#48aaed');
 
 const getColor = (color, opacity = 1, darken = 0) => {
     return color
@@ -16,15 +23,18 @@ export default {
     colors: {
         black: (opacity = 1, darken = 0) => getColor(black, opacity, darken),
         white: (opacity = 1, darken = 0) => getColor(white, opacity, darken),
-        primary: (opacity = 1, darken = 0) => getColor(teal, opacity, darken),
-        primaryInvert: (opacity = 1, darken = 0) =>
-            getColor(white, opacity, darken),
-        light: (opacity = 1, darken = 0) => getColor(white, opacity, darken),
-        lightInvert: (opacity = 1, darken = 0) =>
-            getColor(black, opacity, darken),
-        danger: (opacity = 1, darken = 0) =>
-            getColor(firebrick, opacity, darken),
-        dangerInvert: (opacity = 1, darken = 0) =>
-            getColor(white, opacity, darken),
+        orange: (opacity = 1, darken = 0) => getColor(orange, opacity, darken),
+        lightOrange: (opacity = 1, darken = 0) =>
+            getColor(lightOrange, opacity, darken),
+        darkOrange: (opacity = 1, darken = 0) =>
+            getColor(darkOrange, opacity, darken),
+        dark: (opacity = 1, darken = 0) => getColor(dark, opacity, darken),
+        red: (opacity = 1, darken = 0) => getColor(red, opacity, darken),
+        darkRed: (opacity = 1, darken = 0) =>
+            getColor(darkRed, opacity, darken),
+        gray: (opacity = 1, darken = 0) => getColor(gray, opacity, darken),
+        lightGray: (opacity = 1, darken = 0) =>
+            getColor(lightGray, opacity, darken),
+        azure: (opacity = 1, darken = 0) => getColor(azure, opacity, darken),
     },
 };
