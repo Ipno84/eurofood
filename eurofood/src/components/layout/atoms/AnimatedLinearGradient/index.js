2fb3275a51efc794a8021969811562625b6d571f
Marco Abate <m.abate@logispin.com>
2020-04-18 17:35:12 +0200

    edit animation on skeletons

--- eurofood/src/components/layout/atoms/AnimatedLinearGradient/index.js
+++ eurofood/src/components/layout/atoms/AnimatedLinearGradient/index.js
@@ -1,16 +1,15 @@
 import { Animated, Easing } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
 const AnimatedLinearGradient = () => {
-    const [width, setWidth] = useState(0);
-    let left = new Animated.Value(-200);
+    let opacity = new Animated.Value(0);
     const animateWidth = () => {
         Animated.loop(
-            Animated.timing(left, {
-                toValue: width + 200,
+            Animated.timing(opacity, {
+                toValue: 2,
                 duration: 2000,
                 easing: Easing.ease,
                 useNativeDriver: true
@@ -20,10 +19,9 @@ const AnimatedLinearGradient = () => {
     };
     useEffect(() => {
         animateWidth();
-    }, [width]);
+    }, []);
     return (
         <LinearGradient
-            onLayout={e => setWidth(e.nativeEvent.layout.width)}
             colors={['#e2e2e2', '#e6e6e6']}
             style={{
                 flex: 1,
@@ -31,36 +29,25 @@ const AnimatedLinearGradient = () => {
                 positon: 'relative',
                 overflow: 'hidden'
             }}>
-            <Animated.View
+            <OpacityView
                 style={{
-                    width: 20,
-                    position: 'absolute',
-                    top: 0,
-                    bottom: 0,
-                    transform: [
-                        {
-                            translateX: left
-                        }
-                    ]
-                }}>
-                <Shimmer />
-            </Animated.View>
+                    opacity: opacity.interpolate({
+                        inputRange: [0, 1, 2],
+                        outputRange: [0, 1, 0]
+                    })
+                }}
+            />
         </LinearGradient>
     );
 };
 
 export default AnimatedLinearGradient;
 
-const Shimmer = styled.View`
+const OpacityView = Animated.createAnimatedComponent(styled.View`
     position: absolute;
-    top: 0;
-    bottom: 0;
     left: 0;
-    right: 0;
-    border-radius: 50px;
-    shadow-color: ${({ theme }) => theme.colors.alterGray(1)};
-    shadow-offset: 0 0;
-    shadow-opacity: 1;
-    shadow-radius: 30px;
-    elevation: 30;
-`;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: ${({ theme }) => theme.colors.alterGray(0.6)};
+`);
