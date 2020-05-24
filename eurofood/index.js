c58032a3f7a1a496885e07e559f33c00717a2abc
Marco Abate <m.abate@logispin.com>
2020-05-24 09:48:43 +0200

    init strip and create token

--- eurofood/index.js
+++ eurofood/index.js
@@ -6,9 +6,16 @@ import { AppRegistry, YellowBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import setupAxios from './src/api/setupAxios';
+import stripe from 'tipsi-stripe';
+
+stripe.setOptions({
+    publishableKey: 'pk_test_94TNPj0Fiwql8EwRqoHtkJma00hsXgD5QO',
+    merchantId: 'acct_1GPrWYFcSj0xfSJi',
+    androidPayMode: 'test'
+});
 
 YellowBox.ignoreWarnings([
-    'VirtualizedLists should never be nested', // TODO: Remove when fixed
+    'VirtualizedLists should never be nested' // TODO: Remove when fixed
 ]);
 
 setupAxios();
--- eurofood/src/components/layout/pages/PaymentMethod/PayWithCard.js
+++ eurofood/src/components/layout/pages/PaymentMethod/PayWithCard.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 
-import Container from '../../atoms/Container';
 import { CreditCardInput } from 'react-native-input-credit-card';
 import PlainButton from '../../atoms/Button/PlainButton';
+import Spacer from '../../atoms/Spacer';
 import isSelectedPaymentModuleSelector from '../../../../state/selectors/CheckoutSelectors/isSelectedPaymentModuleSelector';
+import { red } from '../../../../constants/ThemeConstants';
 import stripe from 'tipsi-stripe';
+import styled from 'styled-components/native';
 import { useSelector } from 'react-redux';
 
 const PayWithCard = () => {
@@ -18,12 +20,14 @@ const PayWithCard = () => {
         valid: false,
         values: { number: '', expiry: '', cvc: '', name: '', type: undefined }
     });
+    const [token, setToken] = useState(null);
     const isSelectedPaymentModule = useSelector(state =>
         isSelectedPaymentModuleSelector(state, 'stripe_official')
     );
     if (!isSelectedPaymentModule) return null;
     return (
-        <Container>
+        <>
+            <Spacer top={16} />
             <CreditCardInput
                 requiresName={true}
                 requiresCVC={true}
@@ -40,11 +44,46 @@ const PayWithCard = () => {
                     cvc: 'CVC',
                     name: 'Mario Rossi'
                 }}
+                cardScale={1.1}
+                invalidColor={red.toString()}
                 onChange={e => setCardForm(e)}
             />
-            <PlainButton disabled={!cardForm.valid}>Paga</PlainButton>
-        </Container>
+            <Spacer top={-16} />
+            <Container>
+                <PlainButton
+                    onPress={() => {
+                        console.log(cardForm);
+                        stripe
+                            .createTokenWithCard({
+                                number: cardForm.values.number,
+                                expMonth: Number(
+                                    cardForm.values.expiry.split('/')[0]
+                                ),
+                                expYear: Number(
+                                    cardForm.values.expiry.split('/')[1]
+                                ),
+                                cvc: cardForm.values.cvc,
+                                name: cardForm.values.name
+                            })
+                            .then(success => setToken(success))
+                            .catch(e => console.log('error', e));
+                        // stripe
+                        //     .paymentRequestWithCardForm()
+                        //     .then(e => console.log('success', e))
+                        //     .catch(e => console.log('error', e));
+                    }}
+                    disabled={!cardForm.valid}>
+                    Paga
+                </PlainButton>
+            </Container>
+        </>
     );
 };
 
 export default PayWithCard;
+
+const Container = styled.View`
+    padding-left: 16px;
+    padding-right: 16px;
+    padding-bottom: 16px;
+`;
